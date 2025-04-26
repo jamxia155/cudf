ffc4e736f99b21046713003c2241a9b4c0d87a94
James Xia <jamxia@nvidia.com>
2025-04-26 14:54:26 -0700

    Use PR branch for kvikio

--- cpp/cmake/thirdparty/get_kvikio.cmake
+++ cpp/cmake/thirdparty/get_kvikio.cmake
@@ -19,8 +19,8 @@ function(find_and_configure_kvikio VERSION)
     kvikio ${VERSION}
     GLOBAL_TARGETS kvikio::kvikio
     CPM_ARGS
-    GIT_REPOSITORY https://github.com/rapidsai/kvikio.git
-    GIT_TAG branch-${VERSION}
+    GIT_REPOSITORY https://github.com/joosthooz/kvikio
+    GIT_TAG support-aws-temp-creds
     GIT_SHALLOW TRUE SOURCE_SUBDIR cpp
     OPTIONS "KvikIO_BUILD_EXAMPLES OFF" "KvikIO_REMOTE_SUPPORT ${CUDF_KVIKIO_REMOTE_IO}"
   )
